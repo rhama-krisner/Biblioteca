6b4f54c2f6f15da4279b4a33d4c71a74f8263295
Rhama Krisner <rhamakrisner@gmail.com>
2021-09-21 21:54:19 -0300

    Create Models

--- biblioteca/models.py
+++ biblioteca/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Biblioteca_Model(models.Model):
+    nome = models.CharField(max_length=70)
+    autor = models.CharField(max_length=70)
+    isbn = models.IntegerField(blank=True)
+    ano_publicacao = models.IntegerField(blank=True)
+    genero = models.CharField(max_length=255)
+    idioma = models.CharField(max_length=10, default='Unknown', blank=True)
+    idioma_original = models.CharField(max_length=10, default='Unknown', blank=True)
+    data_cadastro = models.IntegerField(default='Unknown',blank=True)
+
+    def __str__(self):
+        return self.nome
+        

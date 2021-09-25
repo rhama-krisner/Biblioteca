eda07ddb6749a64b1e4e8836be22a66d559a7489
Rhama Krisner <rhamakrisner@gmail.com>
2021-09-25 00:06:19 -0300

    Update views.py

--- biblioteca/views.py
+++ biblioteca/views.py
@@ -1,8 +1,8 @@
+from django.shortcuts import render
+from rest_framework import viewsets
 from biblioteca.serializer import Biblioteca_Serializer
-from rest_framework import viewsets, permissions
 from biblioteca.models import Biblioteca_Model
 
-class Biblioteca_ViewSet(viewsets.ModelViewSet):
+class Biblioteca_Views(viewsets.ModelViewSet):
     queryset = Biblioteca_Model.objects.all()
-    serializer = Biblioteca_Serializer
-    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = Biblioteca_Serializer

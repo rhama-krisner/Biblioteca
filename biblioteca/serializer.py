4a1e0ecfe9e22cd9aca8e687f9727c67ba82b4ad
Rhama Krisner <rhamakrisner@gmail.com>
2021-09-25 00:06:25 -0300

    Update serializer.py

--- biblioteca/serializer.py
+++ biblioteca/serializer.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from biblioteca.models import Biblioteca_Model
 
-class Biblioteca_Serializer(serializers.Biblioteca_Model):
+class Biblioteca_Serializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         models = Biblioteca_Model
         fields = '__all__'

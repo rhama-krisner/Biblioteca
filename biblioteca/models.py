71e019c885c248e82366563120c6ab361296e097
Rhama Krisner <rhamakrisner@gmail.com>
2021-09-25 00:06:30 -0300

    Update models.py

--- biblioteca/models.py
+++ biblioteca/models.py
@@ -1,8 +1,6 @@
 from django.db import models
-import uuid
 
 class Biblioteca_Model(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, null=False, blank=True)
     nome = models.CharField(max_length=70)
     autor = models.CharField(max_length=70)
     isbn = models.IntegerField(blank=True)
@@ -11,7 +9,4 @@ class Biblioteca_Model(models.Model):
     idioma = models.CharField(max_length=10, default='Unknown', blank=True)
     idioma_original = models.CharField(max_length=10, default='Unknown', blank=True)
     data_cadastro = models.IntegerField(default='Unknown',blank=True)
-
-    def __str__(self):
-        return self.nome
         

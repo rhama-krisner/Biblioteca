822663ba26d4a89b4fbde7d706bff2469d4c29e6
Rhama Krisner <rhamakrisner@gmail.com>
2021-09-25 00:05:59 -0300

    Update urls.py

--- setup/urls.py
+++ setup/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('biblioteca/v1/', include('biblioteca.urls', namespace='biblioteca')),
-    path('biblioteca-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
+    path('', include('biblioteca.urls')),
 ]

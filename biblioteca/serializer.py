e077a12ffaadaca67966ee50e02113a324cfc414
Rhama Krisner <rhamakrisner@gmail.com>
2021-09-27 20:33:13 -0300

    Deleted

--- biblioteca/serializer.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from biblioteca.models import Biblioteca_Model
-
-class Biblioteca_Serializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        models = Biblioteca_Model
-        fields = '__all__'
--- biblioteca/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from rest_framework import viewsets
-from biblioteca.serializer import Biblioteca_Serializer
-from biblioteca.models import Biblioteca_Model
-
-class Biblioteca_Views(viewsets.ModelViewSet):
-    queryset = Biblioteca_Model.objects.all()
-    serializer_class = Biblioteca_Serializer

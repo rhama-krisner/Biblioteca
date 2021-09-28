6a305f8b6022764341c9ecdcb0a808da5e0a124f
Rhama Krisner <rhamakrisner@gmail.com>
2021-09-28 19:21:05 -0300

    Update models.py

--- biblioteca/models.py
+++ biblioteca/models.py
@@ -1,2 +1,12 @@
 from django.db import models
 
+class Livro_Model(models.Model):
+    nome_livro = models.CharField(max_length=255, blank=True)
+    isbn = models.CharField(max_length=13, blank=True)
+    autor = models.CharField(max_length=255)
+    ano_publicacao = models.DateField(blank=True)
+    genero = models.CharField(max_length=255)
+    idioma = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.nome_livro

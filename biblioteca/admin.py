75c960fa9ffb8e8779778544dfdc687ecf97d98b
Rhama Krisner <rhamakrisner@gmail.com>
2021-09-28 19:21:27 -0300

    Update admin.py

--- biblioteca/admin.py
+++ biblioteca/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from biblioteca.models import Livro_Model
 
 # Register your models here.
+
+class Biblioteca(admin.ModelAdmin):
+    list_display = ('id','nome_livro','isbn','autor','ano_publicacao','genero','idioma')
+    list_display_links = ('id', 'nome_livro')
+    search_fields = ('nome',)
+    list_per_page = 20
+
+admin.site.register(Livro_Model, Biblioteca)

4cf5c440edfacdc8636c606a1f6633463ae10531
Rhama Krisner <rhamakrisner@gmail.com>
2021-09-28 19:21:17 -0300

    Update settings.py

--- setup/settings.py
+++ setup/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'biblioteca',
     'rest_framework',
+    'biblioteca',
 ]
 
 MIDDLEWARE = [
